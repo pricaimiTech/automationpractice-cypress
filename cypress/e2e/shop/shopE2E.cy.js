51ace99a11e42697e2992d0c9cd7b9ec9cd37592
Priscila Caimi <priscila.caimi@hotmail.com>
2024-08-14 21:31:38 -0300

    finalização do teste

--- cypress/e2e/shop/shopE2E.cy.js
+++ cypress/e2e/shop/shopE2E.cy.js
@@ -11,14 +11,39 @@ describe('Sign In', () => {
     cy.visit('http://www.automationpractice.pl/index.php')
   })
   
-  it('Search for a dress', () => {
+  it.skip('Search for a dress', () => {
     cy.get(header.search).type("Printed evening dress").type('{ENTER}')
     cy.get("div[id=center_column]  ul[class='product_list grid row']")
     .find("li div[class=product-image-container]")
     .click()
   });
 
-  it.skip('Visit dress page', () => {
+  it('Visit dress page', () => {
     cy.visit('http://www.automationpractice.pl/index.php?id_product=4&controller=product&search_query=Printed+evening+dress&results=1')
+    cy.get("span[id=availability_value]").should('be.visible')
+
+    const textLabel = "In stock"
+    const spanText = cy.get("span[id=availability_value]").invoke("text")
+
+    if(spanText == textLabel) {
+      console.log("pode comprar o produto")
+    }else {
+      console.log("procurar o produto disponivel")
+      cy.get("ul[id=color_to_pick_list]")
+      .find("li[class=selected]") // procurar uma forma de adicionar o li que não contem a classe select
+      .next()
+      .click()
+
+      cy.get("p[id=add_to_cart] ")
+      .find("button")
+      .click()
+
+      cy.get("div[id=layer_cart]")
+      .invoke('show')
+      .find("div div span")
+      .first()
+      .next()
+      .contains('Product successfully added to your shopping cart')
+    }
   });
 })
